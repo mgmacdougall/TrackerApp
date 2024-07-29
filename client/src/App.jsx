24a1a8d218300e53d5a6fdddbd36ad994ebeb9c9
Michael MacDougall <mgmacdougall@outlook.com>
2024-07-29 14:50:55 -0400

    Add of log bug functionality, add favicon and updated ticket logging form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,15 +14,35 @@ function App() {
   const[userPassword, setUserPassword] = useState("");
   const[allTickets, setAllTickets] = useState([]);
 
-// const navigate = useNavigate();
+  const[issueTitle, setIssueTitle]=useState("");
+  const[issueDescription, setIssueDescription]=useState("");
+  const[component, setComponent]= useState("");
+  const[issueState, setIssueState]= useState("");
+  const[issueAssignee, setIssueAssignee]= useState("");
+  const[issueSteps, setIssueSteps]=useState("");
 
   const handleEmailInput=(e)=>{ setUserName(e.target.value); console.log(userName)};
   const handlePasswordInput=(e)=> setUserPassword(e.target.value);
+
   const handleLogInClick=(e)=>{
     e.preventDefault();
     setIsLoggedIn(true);
     console.log('here')
   }
+
+  const handleTicketSubmit=(e)=>{
+    e.preventDefault();
+    console.log('here', issueTitle, component);
+    axios.post("http://localhost:4044/ticket",{title: issueTitle}).then(response=> console.log("success",response)).catch(e=> console.log('failed',e))
+    
+  }
+
+  const handleIssueTitle=e=> setIssueTitle(e.target.value);
+  const handleIssueDescription=e=> setIssueDescription(e.target.value);
+  const handleComponentSelection=e=> {setComponent(e.target.value); console.log(e.target.value)};
+  const handleIssueState=e=> {setIssueState(e.target.value); console.log(e.target.value)}
+  const handleIssueSteps=e=> setIssueSteps(e.target.value);
+  const handleIssueAssignee=e=> {setIssueAssignee(e.target.value); console.log(e.target.value)}
 
   useEffect(() => {
     axios.get('http://localhost:4044/ticket/all').then(response=> setAllTickets(response.data)).catch(e=> console.log(e))
@@ -32,7 +52,7 @@ function App() {
     <>
     <Routes>
         <Route path="/" element={<HomePage tickets={allTickets} display={isLoggedIn}/>}/> 
-        <Route path="/tickets" element={<TicketPage/>}/> 
+        <Route path="/tickets" element={<TicketPage handlers={[handleTicketSubmit, handleIssueTitle,handleIssueDescription,handleComponentSelection,handleIssueState,handleIssueSteps,handleIssueAssignee] } vals={[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]} />}/> 
         <Route path="/admin" element={<AdminPage/>}/> 
         <Route path="/login" element={<LogInForm emailHandler={handleEmailInput} passwordHandler={handlePasswordInput} handleSubmit={handleLogInClick} uName={userName} uPass={userPassword}/>}/> 
       </Routes>
--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -4,47 +4,63 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Navbar from '../components/Navbar';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-function TicketPage() {
+function TicketPage({handlers,vals}) {
+const[handleTicketSubmit,handleIssueTitle,handleIssueDescription,handleComponentSelection,handleIssueState,handleIssueSteps,handleIssueAssignee]=handlers;
+const[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]=vals;
   return (
     <>
     <Navbar/>
     <h1 className="text-centered">Bug details</h1>
-    <Form className='align-center max-width-large'>
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Title</Form.Label>
-        <Form.Control placeholder="Bug title" />
+    <Form className='align-center max-width-large'  onSubmit={(e)=>handleTicketSubmit(e)}>
+      <Form.Group className="mb-3" controlId="issueTitle">
+        <Form.Label>Bug title</Form.Label>
+        <Form.Control val={issueTitle}  placeholder="Bug title" onChange={e=> handleIssueTitle(e)}/>
       </Form.Group>
- <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="issueComponent">
           <Form.Label>Component</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
+          <Form.Select onChange={(e)=> handleComponentSelection(e)} defaultValue="Choose...">
+            <option value={""}>Choose...</option>
+            <option value={"loginPage"}>Login Page</option>
+            <option value={"homePage"}>Home Page</option>
+            <option value={"adminPage"}>Admin Page</option>
+            <option value={"ticketPage"}>Ticket Page</option>
           </Form.Select>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridState">
+        <Form.Group as={Col} controlId="issueState">
           <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
+          <Form.Select defaultValue="Choose..." onChange={(e=> handleIssueState(e))}>
             <option>Choose...</option>
-            <option>...</option>
+            <option value={"new"}>New</option>
+            <option value={"active"}>Active</option>
+            <option value={"fixed"}>Fixed</option>
+            <option value={"tested"}>Tested</option>
+            <option value={"closed"}>Closed</option>
           </Form.Select>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Status</Form.Label>
-          <Form.Control />
+        <Form.Group as={Col} controlId="issueAssignee">
+          <Form.Label>Owner</Form.Label>
+           <Form.Select defaultValue="Choose..." onChange={(e=> handleIssueAssignee(e))}>
+            <option>Choose...</option>
+            <option value={"mmike"}>Mike</option>
+            <option value={"ted"}>Ted</option>
+            <option value={"jen"}>Jen</option>
+          </Form.Select>
         </Form.Group>
       </Row>
-      <FloatingLabel controlId="floatingTextarea2" label="Comments">
+
+      <FloatingLabel controlId="issueSteps" label="Steps">
         <Form.Control
           as="textarea"
-          placeholder="Leave a comment here"
+          placeholder="Steps"
           style={{ height: '100px' }}
+          onChange={e=> handleIssueDescription(e)}
+          value={issueDescription}
         />
       </FloatingLabel>
 
-     
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Default file input example</Form.Label>
         <Form.Control type="file" />
@@ -52,9 +68,9 @@ function TicketPage() {
       <Button variant="primary" type="submit">
         Submit
       </Button>
-      <Button variant="primary" type="submit">
+      {/* <Button variant="primary" type="submit">
         Cancel
-      </Button>
+      </Button> */}
     </Form>
     </>
   );
