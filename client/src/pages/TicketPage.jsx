29f4b991fcc732b5c7d24194027b922e976b4f28
Michael MacDougall <mgmacdougall@outlook.com>
2024-07-30 09:47:32 -0400

    Refactor of TicketPage

--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -5,21 +5,21 @@ import Row from 'react-bootstrap/Row';
 import Navbar from '../components/Navbar';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 function TicketPage({handlers,vals}) {
-const[handleTicketSubmit,handleIssueTitle,handleIssueDescription,handleComponentSelection,handleIssueState,handleIssueSteps,handleIssueAssignee]=handlers;
-const[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]=vals;
+const[handleTicketSubmit,handleFormChange]=handlers;
+const[form]=vals;
   return (
     <>
     <Navbar/>
     <h1 className="text-centered">Bug details</h1>
-    <Form className='align-center max-width-large'  onSubmit={(e)=>handleTicketSubmit(e)}>
+    <Form className='align-center max-width-large' onSubmit={(e)=>handleTicketSubmit(e)}>
       <Form.Group className="mb-3" controlId="issueTitle">
         <Form.Label>Bug title</Form.Label>
-        <Form.Control val={issueTitle}  placeholder="Bug title" onChange={e=> handleIssueTitle(e)}/>
+        <Form.Control val={form.title} name="title" placeholder="Bug title" onChange={e=> handleFormChange(e)}/>
       </Form.Group>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="issueComponent">
           <Form.Label>Component</Form.Label>
-          <Form.Select onChange={(e)=> handleComponentSelection(e)} defaultValue="Choose...">
+          <Form.Select name="component" onChange={(e)=> handleFormChange(e)}  defaultValue="Choose...">
             <option value={""}>Choose...</option>
             <option value={"loginPage"}>Login Page</option>
             <option value={"homePage"}>Home Page</option>
@@ -30,7 +30,7 @@ const[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]
 
         <Form.Group as={Col} controlId="issueState">
           <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose..." onChange={(e=> handleIssueState(e))}>
+          <Form.Select name="state" defaultValue="Choose..." onChange={(e=> handleFormChange(e))} >
             <option>Choose...</option>
             <option value={"new"}>New</option>
             <option value={"active"}>Active</option>
@@ -42,8 +42,8 @@ const[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]
 
         <Form.Group as={Col} controlId="issueAssignee">
           <Form.Label>Owner</Form.Label>
-           <Form.Select defaultValue="Choose..." onChange={(e=> handleIssueAssignee(e))}>
-            <option>Choose...</option>
+           <Form.Select name="owner" defaultValue="Choose..." onChange={(e=> handleFormChange(e))} >
+            <option value="">Choose...</option>
             <option value={"mmike"}>Mike</option>
             <option value={"ted"}>Ted</option>
             <option value={"jen"}>Jen</option>
@@ -56,8 +56,9 @@ const[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]
           as="textarea"
           placeholder="Steps"
           style={{ height: '100px' }}
-          onChange={e=> handleIssueDescription(e)}
-          value={issueDescription}
+          onChange={e=> handleFormChange(e)}
+          value={form.steps}
+          name="steps"
         />
       </FloatingLabel>
 
