788e521cf632c37920b0afa0306069a406b5bdd8
Michael MacDougall <mgmacdougall@outlook.com>
2024-07-30 09:47:13 -0400

    Refactor of the way the TicketPage is handled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import LogInForm from './pages/LoginPage'
 import HomePage from './pages/HomePage';
 import TicketPage from './pages/TicketPage';
 import AdminPage from './pages/AdminPage';
-import { Routes, Route, Navigate  } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
@@ -14,12 +14,13 @@ function App() {
   const[userPassword, setUserPassword] = useState("");
   const[allTickets, setAllTickets] = useState([]);
 
-  const[issueTitle, setIssueTitle]=useState("");
-  const[issueDescription, setIssueDescription]=useState("");
-  const[component, setComponent]= useState("");
-  const[issueState, setIssueState]= useState("");
-  const[issueAssignee, setIssueAssignee]= useState("");
-  const[issueSteps, setIssueSteps]=useState("");
+  const[form, setForm]=useState({
+    title:'',
+    component:'',
+    state:'',
+    owner:'',
+    steps:''
+  });
 
   const handleEmailInput=(e)=>{ setUserName(e.target.value); console.log(userName)};
   const handlePasswordInput=(e)=> setUserPassword(e.target.value);
@@ -32,17 +33,16 @@ function App() {
 
   const handleTicketSubmit=(e)=>{
     e.preventDefault();
-    console.log('here', issueTitle, component);
-    axios.post("http://localhost:4044/ticket",{title: issueTitle}).then(response=> console.log("success",response)).catch(e=> console.log('failed',e))
+    axios.post("http://localhost:4044/ticket",{title: form.title}).then(response=> console.log("success",response)).catch(e=> console.log('failed',e))
     
   }
 
-  const handleIssueTitle=e=> setIssueTitle(e.target.value);
-  const handleIssueDescription=e=> setIssueDescription(e.target.value);
-  const handleComponentSelection=e=> {setComponent(e.target.value); console.log(e.target.value)};
-  const handleIssueState=e=> {setIssueState(e.target.value); console.log(e.target.value)}
-  const handleIssueSteps=e=> setIssueSteps(e.target.value);
-  const handleIssueAssignee=e=> {setIssueAssignee(e.target.value); console.log(e.target.value)}
+  const handleFormChange= e=>{
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
 
   useEffect(() => {
     axios.get('http://localhost:4044/ticket/all').then(response=> setAllTickets(response.data)).catch(e=> console.log(e))
@@ -52,10 +52,11 @@ function App() {
     <>
     <Routes>
         <Route path="/" element={<HomePage tickets={allTickets} display={isLoggedIn}/>}/> 
-        <Route path="/tickets" element={<TicketPage handlers={[handleTicketSubmit, handleIssueTitle,handleIssueDescription,handleComponentSelection,handleIssueState,handleIssueSteps,handleIssueAssignee] } vals={[issueTitle,issueDescription,component,issueState,issueAssignee,issueSteps]} />}/> 
+        <Route path="/tickets" element={<TicketPage handlers={[handleTicketSubmit, handleFormChange] } vals={[form]} />}/> 
         <Route path="/admin" element={<AdminPage/>}/> 
         <Route path="/login" element={<LogInForm emailHandler={handleEmailInput} passwordHandler={handlePasswordInput} handleSubmit={handleLogInClick} uName={userName} uPass={userPassword}/>}/> 
       </Routes>
+      
     </>
   )
 }
