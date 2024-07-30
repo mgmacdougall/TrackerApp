9291e6638e31444e87aeac1358b7551bad289614
Michael MacDougall <mgmacdougall@outlook.com>
2024-07-30 11:30:09 -0400

    Filled out TicketModel correctly holding all expected values

--- api/src/models/TicketModel.js
+++ api/src/models/TicketModel.js
@@ -1,7 +1,11 @@
 import {model, Schema} from 'mongoose';
 
 const ticketSchema = new Schema({
-  title: String
+  title: String,
+  component: String,
+  state: String,
+  owner: String,
+  steps: String
 });
 
 const TicketModel = model('Ticket', ticketSchema);
