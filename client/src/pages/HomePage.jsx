7a8aeede66d03465a98c0171e16f868338cba569
Michael MacDougall <mgmacdougall@outlook.com>
2024-08-01 16:02:32 -0400

    Add of debug point

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import NavBar from '../components/Navbar';
 
 function HomePage({display}) {
-  console.log(display)
   return (
     <>
     <NavBar/>
       <h1 className='text-centered'>Welcome to the Bug Tracker application</h1>  
-
     </>)
 }
 
